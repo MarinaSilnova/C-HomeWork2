48291f2c73a0fa64bccc8dd71214f6fea9d86153
Marina Silnova <marusia421@mail.ru>
2022-09-08 11:59:49 +0300

    add ex1option1

---
+++ ex10option1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 10 вариант решения первый;
+int ThreeDigitNumber()
+{
+    Console.WriteLine("Введите 3-х значное число");
+    int n = Convert.ToInt32(Console.ReadLine());
+    while(n < 100 || n > 999)
+    {
+        Console.Write(" Repeat, please: ");
+        n = Convert.ToInt32(Console.ReadLine());
+    }
+    return n;
+}
+void SecondDigit()
+{
+    int n = ThreeDigitNumber();
+    int a = n / 10;
+    Console.WriteLine(a % 10);
+}
+SecondDigit();
